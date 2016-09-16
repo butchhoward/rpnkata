967c7baf941d3dbab1db6908d460fe15fbe6a814
Butch Howard <butchhoward@gmail.com>
2016-09-16 02:07:14 -0400

    other simple paren

--- rpnlib-test.c
+++ rpnlib-test.c
@@ -16,6 +16,7 @@ struct infix_to_postfix {
     ,{.infix="a*c+d", .postfix="ac*d+"}
     ,{.infix="a+c*d", .postfix="acd*+"}
     ,{.infix="(a+c)*d", .postfix="ac+d*"}
+    ,{.infix="a+(c*d)", .postfix="acd*+"}
 };
 
 START_TEST(when_infix_is_then_postfix_is)
